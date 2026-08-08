e0667a49357f913dc3ba8689d1f9c672889442af
Duc854 <nmduc854@gmail.com>
2026-08-08 11:02:28 +0100

    refactor: add enrollment constraint

---
+++ AILA.Infrastructure/Persistence/Migrations/20260808100146_EnrollmentConstraint.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AILA.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnrollmentConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Enrollments_LearnerId",
+                table: "Enrollments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollments_LearnerId_CourseId",
+                table: "Enrollments",
+                columns: new[] { "LearnerId", "CourseId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Enrollments_LearnerId_CourseId",
+                table: "Enrollments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollments_LearnerId",
+                table: "Enrollments",
+                column: "LearnerId");
+        }
+    }
+}
