72799bf3be7559cfd94e53f06484f9bc923a1315
Duc854 <nmduc854@gmail.com>
2026-08-08 10:39:59 +0100

    refactor: add relationship for course report content

---
+++ AILA.Infrastructure/Persistence/Migrations/20260808093855_UpdateContentReportRelationshipCourse.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AILA.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateContentReportRelationshipCourse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ContentReport_Courses_CourseId",
+                table: "ContentReport");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ContentReport_Courses_CourseId",
+                table: "ContentReport",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ContentReport_Courses_CourseId",
+                table: "ContentReport");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ContentReport_Courses_CourseId",
+                table: "ContentReport",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
